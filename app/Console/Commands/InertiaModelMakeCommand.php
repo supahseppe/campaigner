80755411b4e798656f9ab34fe730e19be803092a
Giuseppe <giuseppe.daristotile@golfchannel.com>
2020-01-24 02:43:54 -0500

    Attempting to fix dummy replace

--- app/Console/Commands/InertiaModelMakeCommand.php
+++ app/Console/Commands/InertiaModelMakeCommand.php
@@ -136,7 +136,7 @@ class InertiaModelMakeCommand extends ModelMakeCommand
      */
     protected function replaceDummy($file, $name)
     {
-        return str_replace(
+        return preg_replace(
             ['Dummy', 'Dummies', 'dummy', 'dummies'],
             [Str::studly($name), Str::pluralStudly($name), Str::lower($name), Str::plural(Str::lower($name))],
             $file
